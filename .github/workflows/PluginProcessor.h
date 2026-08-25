7e8691470b889fe477b027431a34a8d73c68286d
lattemancellotti <matte.lancellotti@gmail.com>
2026-08-26 00:23:10 +0200

    Add PluginProcessor.h for audio processing class

---
+++ .github/workflows/PluginProcessor.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <JuceHeader.h>
+
+class TheMogwaisTouchAudioProcessor  : public juce::AudioProcessor
+{
+public:
+    TheMogwaisTouchAudioProcessor();
+    ~TheMogwaisTouchAudioProcessor() override;
+
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override { return true; }
+
+    const juce::String getName() const override { return "The Mogwai's Touch"; }
+    bool acceptsMidi() const override { return false; }
+    bool producesMidi() const override { return false; }
+    double getTailLengthSeconds() const override { return 0.0; }
+
+    int getNumPrograms() override { return 1; }
+    int getCurrentProgram() override { return 0; }
+    void setCurrentProgram (int) override {}
+    const juce::String getProgramName (int) override { return {}; }
+    void changeProgramName (int, const juce::String&) override {}
+
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    juce::AudioProcessorValueTreeState apvts;
+
+private:
+    juce::AudioProcessorValueTreeState::ParameterLayout createParameters();
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TheMogwaisTouchAudioProcessor)
+};
